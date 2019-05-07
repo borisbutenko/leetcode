db788e68de1e6a4b6da58dd26128ec8d44d2c901
borisbutenko <iamborisbutenko@gmail.com>
2019-05-07 18:53:50 +0400

    update fizz_buzz.py

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,10 +1,11 @@
-def fizz_buzz(max_num: int = 100):
+def fizz_buzz(max_num: int = 100) -> list:
     """`FizzBuzz classic <https://en.wikipedia.org/wiki/Fizz_buzz>`
 
     :param max_num: {int} max number for FizzBuzz loop
     """
 
     max_num += 1
+    lst = []
 
     for i in range(0, max_num):
         text = ""
@@ -14,4 +15,9 @@ def fizz_buzz(max_num: int = 100):
         if i % 5 == 0:
             text += "Buzz"
 
-        print(text or i)
+        lst.append(text or i)  # or print(text or i)
+
+    return lst
+
+
+print(fizz_buzz())  # or fizz_buzz if not return the list
